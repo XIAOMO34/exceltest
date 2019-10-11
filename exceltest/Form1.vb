5626f5768cecc6b1869bfab9ee5d75433f2a096b
LJX <LJX@DESKTOP-CG173CH>
2019-10-11 11:05:05 +0800

    2019

--- exceltest/Form1.vb
+++ exceltest/Form1.vb
@@ -2,48 +2,44 @@
     Dim myexcel As Microsoft.Office.Interop.Excel.Application
     Dim myworkbook As Microsoft.Office.Interop.Excel.Workbook
     Dim myworksheet As Microsoft.Office.Interop.Excel.Worksheet
+    Dim myword As Microsoft.Office.Interop.Word.Application
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Try
             myexcel = CType(GetObject(, "Excel.Application"), Microsoft.Office.Interop.Excel.Application)
             ''获得已经打开的EXCEL对象
             Step21()
-            Step3()
+            Wmassout()
+            'Step3()
+            Me.Close()
         Catch ex As Exception
             Step22()
-            Step3()
         End Try
     End Sub
     Function Step21()  ''对已经打开的表格操作,此处用函数不用进程来选择性调用！！
         myexcel.WindowState = -4137 ''全屏
         myworkbook = myexcel.ActiveWorkbook
-        myworksheet = myworkbook.Worksheets("原始数据1")
+        myworksheet = myworkbook.Worksheets("Sheet1")
     End Function
     Function Step22() ''重新打开EXCEL顶级对象APPLICATION
         myexcel = CreateObject("Excel.Application")
         myexcel.Visible = True
         myexcel.WindowState = -4137 ''全屏
-        myworkbook = myexcel.Workbooks.Open("D:\腾讯文件\原始数据\文件1.xlsx")
-        myworksheet = myworkbook.Worksheets("原始数据1")
+        Wmassout()
     End Function
-    Function Step3() ''进入EXCEL工作簿操作
-        ''数学竞赛处理数据
-        Dim i As Long
-        Dim j As Long
-        For i = 7081 To 150000
-            If (myexcel.Range("O" & (i + 1)).Value - myexcel.Range("O" & i).Value) <> 1 And
-                (myexcel.Range("O" & (i + 1)).Value - myexcel.Range("O" & i).Value) <> -86399 Then
-                myexcel.Range("O" & (i + 1)).Select()
-                For j = i + 1 To i + 1000
-                    If myexcel.Range("B" & j).Value = 0 Then
-                        myexcel.Rows((i) & ":" & (j - 1)).Delete(-4162)
-                        ''myexcel.Range("B" & j).EntireRow.Delete()
-                        ''j = j - 1
-
-                        Exit For
-                    End If
-                Next
+    Function Wmassout() ''读取Wmass文件中信息
+        'myworkbook = myexcel.Workbooks.Open("C:\Users\LJX\Desktop\新建 Microsoft Excel 工作表.xlsx")
+        'myworksheet = myworkbook.Worksheets("Sheet1")
+        Dim REG As Microsoft.Office.Interop.Excel.Range
+        REG = myworksheet.Range("A1:A500")
+        For Each i In REG
+            If i.VALUE Like "* 恒载质量    活载质量*" Then ''关键词和通配符
+                'MessageBox.Show(i.ROW + 2)
+                myworksheet.Range("A" & （i.ROW + 2) & ": A" & (i.ROW + 3)).Copy()
+                myworksheet.Range("B1").Select()
+                myworkbook.ActiveSheet.PASTE
             End If
         Next
-        Me.Close()
+        myword = CreateObject("Word.application")
+
     End Function
 End Class
